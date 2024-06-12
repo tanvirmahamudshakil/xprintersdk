7876ec1b61db6a3ee9a11cec4978be7388d32260
tanvirmahamudshakil <shakilhassan887@gmail.com>
2024-06-13 00:32:23 +0600

    update

--- android/src/main/kotlin/com/example/xprintersdk/PrinterService/printerservice.kt
+++ android/src/main/kotlin/com/example/xprintersdk/PrinterService/printerservice.kt
@@ -203,7 +203,7 @@ class printerservice(mcontext: Context, morderModel: OrderData, businessdata: Bu
                     for (section2 in section.components) {
                         if (section2?.product?.shortName?.uppercase() != "NONE") {
                             _comName += " -> " + section2?.product?.shortName;
-                            price += section2?.netAmount ?: 0.0;
+                            price += ((section2?.netAmount ?: 0.0) * (section2?.unit ?: 1));
                         }
                     }
 
