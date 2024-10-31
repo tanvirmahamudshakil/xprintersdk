4f91a389e71bd2ced9d5bc19d8fcbb41b3bff86f
tanvirmahamudshakil <shakilhassan887@gmail.com>
2024-11-01 01:29:28 +0600

    update

--- android/src/main/kotlin/com/example/xprintersdk/PrinterService/printerservice.kt
+++ android/src/main/kotlin/com/example/xprintersdk/PrinterService/printerservice.kt
@@ -128,8 +128,8 @@ class printerservice(mcontext: Context, morderModel: OrderData, businessdata: Bu
         view.draw(canvas)
 
               if(businessdatadata.selectPrinter!!.lowercase() == "label_printer") {
-                  val widthInPixels = 450 // Adjust based on actual DPI and paper width
-                  val heightInPixels = 180
+                  val widthInPixels = 400
+                  val heightInPixels = 170
                 return Bitmap.createScaledBitmap(returnedBitmap, widthInPixels, heightInPixels, true)
               }else{
                   var bitmap: Bitmap = if (businessdatadata.paperSize == 80) {
